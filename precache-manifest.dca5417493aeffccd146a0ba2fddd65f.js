61fe3049d87237392b8bfbd41ba38b43f1c2a802
Azhar-git <60664855+Azhar-git@users.noreply.github.com>
2020-02-23 19:39:31 +0530

    Updates

---
+++ precache-manifest.dca5417493aeffccd146a0ba2fddd65f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652ed30cd988edae48b3c232e2fdab85",
+    "url": "/nzsecservices/index.html"
+  },
+  {
+    "revision": "e3b1c9a22760098fb49d",
+    "url": "/nzsecservices/static/css/2.844c13d2.chunk.css"
+  },
+  {
+    "revision": "a18f94e0867bf3a84c2f",
+    "url": "/nzsecservices/static/css/main.3ba6e329.chunk.css"
+  },
+  {
+    "revision": "e3b1c9a22760098fb49d",
+    "url": "/nzsecservices/static/js/2.874f82b8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/nzsecservices/static/js/2.874f82b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a18f94e0867bf3a84c2f",
+    "url": "/nzsecservices/static/js/main.a442511d.chunk.js"
+  },
+  {
+    "revision": "2df1098660d2886df8d7",
+    "url": "/nzsecservices/static/js/runtime-main.7dc1e33e.js"
+  },
+  {
+    "revision": "d1dba2502f035700dd9d402d20948fd8",
+    "url": "/nzsecservices/static/media/Industry.d1dba250.jpg"
+  },
+  {
+    "revision": "be00ea6a79c7ae6a8abfebcc5c3c3927",
+    "url": "/nzsecservices/static/media/Logo2.be00ea6a.jpeg"
+  },
+  {
+    "revision": "5342d8860b282f3474515da4f3f83186",
+    "url": "/nzsecservices/static/media/Service.5342d886.png"
+  },
+  {
+    "revision": "7b539128643eda0437d2bc9e583a9571",
+    "url": "/nzsecservices/static/media/building.7b539128.jpg"
+  },
+  {
+    "revision": "8f9d458fc6d38f8a184a198fb055e58c",
+    "url": "/nzsecservices/static/media/cctv.8f9d458f.jpg"
+  }
+]);
